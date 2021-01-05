59e2a938c012931347d064c3d0969823e19d4b87
Steven Israel <malenchite@gmail.com>
2021-01-05 11:51:27 -0500

    Query current weather on input, click, or refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
 var cityHistory = [];
 var maxHistory = 10;
+var weatherKey = "5efd05a754597abdb5bf0d33ada633b7";
 
 /* Renders city history list */
 function renderHistory() {
   $("#history-container").empty();
+  /* Adds one div for each city in history */
   cityHistory.forEach(function (city) {
     $("#history-container").append("<div class='past-city'>" + city + "</div>");
-  })
+  });
+  /* Adds click event to run a weather query when a city is clicked */
+  $(".past-city").click(cityFromHistory);
 }
 
 /* Loads city history from local storage */
@@ -16,6 +20,13 @@ function loadHistory() {
   if (loadedHistory) {
     cityHistory = loadedHistory;
     renderHistory();
+  }
+
+  /* Query most recent city or hide results if none */
+  if (cityHistory[0]) {
+    queryCity(cityHistory[0]);
+  } else {
+    $("#result-col").addClass("hidden");
   }
 }
 
@@ -29,18 +40,45 @@ function saveCity(city) {
   }
 }
 
-/* Queries weather and saves city to history */
-function queryCity(event) {
+/* Queries weather from history */
+function cityFromHistory() {
+  var city = $(this).text();
+  queryCity(city);
+}
+
+/* Queries weather from input field and saves city to history */
+function cityFromInput(event) {
   event.preventDefault();
   var city = $("#city-input").val();
-  $("#city-input").val("");
 
-  /* TODO: Send AJAX query to Open Weather API */
+  if (city !== "") {
+    $("#city-input").val("");
+    queryCity(city);
+    saveCity(city);
+  }
+}
 
-  saveCity(city);
+/* Queries a city and updates the current weather. It then queries the lat/lon of that city for UVI and forecase */
+function queryCity(city) {
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + weatherKey;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    $("#result-col").removeClass("hidden");
+    $("#current-date").text(moment().format("M/D/YYYY"));
+    $("#current-city").text(response.name);
+    $("#current-temp").text(response.main.temp);
+    $("#current-humid").text(response.main.humidity);
+    $("#current-wind").text(response.wind.speed);
+    $("#current-icon").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
+
+    /* TODO: Use lat/lon to query UVI and 5-day forecast */
+  });
 }
 
 /* Add click response to search button */
-$("#search-btn").click(queryCity);
+$("#search-btn").click(cityFromInput);
 
 loadHistory();
