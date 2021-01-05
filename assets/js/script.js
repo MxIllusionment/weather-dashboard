ebf12b34ce1f9e7ef4498c11aaa879541f07815c
Steven Israel <malenchite@gmail.com>
2021-01-05 12:12:09 -0500

    Implemented second weather query including UVI update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 var cityHistory = [];
 var maxHistory = 10;
 var weatherKey = "5efd05a754597abdb5bf0d33ada633b7";
+var uviColors = {
+  low: "#289500",
+  moderate: "#f7e400",
+  high: "#f85900",
+  veryHigh: "#d80010",
+  extreme: "#6b49c8"
+};
 
 /* Renders city history list */
 function renderHistory() {
@@ -58,6 +65,48 @@ function cityFromInput(event) {
   }
 }
 
+/* Updates UVI entry with text and color */
+function updateUVI(uvi) {
+  var uviFloat = parseFloat(uvi);
+  var bgColor;
+  var textColor = "white";
+  $("#current-uvi").text(uvi);
+  if (uviFloat < 3) {
+    bgColor = uviColors.low;
+  } else if (uviFLoat < 6) {
+    bgColor = uviColors.moderate;
+    textColor = "black";
+  } else if (uviFloat < 8) {
+    bgColor = uviColors.high;
+  } else if (uviFLoat < 11) {
+    bgColor = uviColors.veryHigh;
+  } else {
+    bgColor = uviColors.extreme;
+  }
+  $("#current-uvi").css("background-color", bgColor);
+  $("#current-uvi").css("color", textColor);
+}
+
+/* TODO: Updates the forecast info for a particular day */
+function updateForecast(day, data) {
+
+}
+
+/* Queries Open Weather "One Call" to retrieve UVI and 5-day forecast */
+function queryOneCall(lon, lat) {
+  var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=" + weatherKey;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    updateUVI(response.current.uvi);
+    for (var i = 0; i < 5; i++) {
+      updateForecast(i + 1, response.daily[i]);
+    }
+  });
+}
+
 /* Queries a city and updates the current weather. It then queries the lat/lon of that city for UVI and forecase */
 function queryCity(city) {
   var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + weatherKey;
@@ -74,7 +123,8 @@ function queryCity(city) {
     $("#current-wind").text(response.wind.speed);
     $("#current-icon").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
 
-    /* TODO: Use lat/lon to query UVI and 5-day forecast */
+    /* Use lat/lon to query UVI and 5-day forecast */
+    queryOneCall(response.coord.lon, response.coord.lat);
   });
 }
 
